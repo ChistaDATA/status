71634acc67a6d1838fe3f14c1ffdbc3f1df224f3
vishal-joshy <1812-vishal@protonmail.com>
2023-11-23 17:41:17 +0530

    Add chistadata colors and icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-// import Image from 'next/image'
 import IncidentsSection from "../src/incidents"
 import ServicesSection from "../src/services"
 
@@ -7,9 +6,9 @@ const Home: NextPage = () => {
   return (
     <div className='h-full w-full '>
       <div className="mt-20 absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-      <div className="w-full h-40 absolute bg-light-purple dark:purple dark:bg-black">
-        <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full bg-purple-500 dark:bg-black">
-          {/* <Image src="/chistadata.svg" width={250} height={150} className="w-40 h-16" alt="Tailwind Play" /> */}
+      <div className="w-full h-40 absolute bg-[#0141A1] dark:purple dark:bg-black">
+        <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-60 md:pr-60 sm:w-full h-full bg-purple-500 dark:bg-black">
+          <img src="/chistadata.svg" className="w-72 h-16" alt="Tailwind Play" />
         </div>
       </div>
       <div className='mt-20 w-full absolute overflow-scroll	'>
