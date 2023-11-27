f364f1171e2ee199dc0a6197129aec4412299cbb
jhonis <jhonis.souza@gmail.com>
2023-11-27 13:57:41 -0300

    Format/ident monitoring_urls object

--- public/env.js
+++ public/env.js
@@ -1,18 +1,9 @@
 window.api_host = 'http://localhost:9090';
 window.monitoring_urls = [
-    {
-        name: "HomePage",
-        url: "https://dev.cloud.chistadata.io"
-    },
-    {
-        name: "Portal",
-        url: "https://portal.dev.cloud.chistadata.io"
-    },
-    {
-        name: "ControlPlane",
-        url: "https://control-plane.dev.cloud.chistadata.io/health"
-    },
-    { name: "Authentication", url: "https://auth.dev.cloud.chistadata.io" },
-    { name: "Keycloak", url: "https://keycloak.dev.cloud.chistadata.io" },
-    { name: "Docs", url: "https://docs.dev.cloud.chistadata.io" }
+    {name: "HomePage", url: "https://dev.cloud.chistadata.io"},
+    {name: "Portal", url: "https://portal.dev.cloud.chistadata.io"},
+    {name: "ControlPlane", url: "https://control-plane.dev.cloud.chistadata.io/health"},
+    {name: "Authentication", url: "https://auth.dev.cloud.chistadata.io"},
+    {name: "Keycloak", url: "https://keycloak.dev.cloud.chistadata.io"},
+    {name: "Docs", url: "https://docs.dev.cloud.chistadata.io"}
 ]
